e1d82d3b030e03ee018c11aeabc812609e931498
Yash Kothari <yashkoth@andrew.cmu.edu>
2024-03-20 14:14:05 -0400

    added store and integrated with pipeline

--- datafusion-integration/src/main.rs
+++ datafusion-integration/src/main.rs
@@ -3,7 +3,11 @@ use datafusion::error::{DataFusionError, Result};
 use datafusion::execution::context::SessionState;
 use datafusion::physical_plan::displayable;
 
+use datafusion::physical_plan::test::exec;
+use datafusion::physical_plan::ExecutionPlan;
 use datafusion::prelude::*;
+use std::sync::Arc;
+use vayu::VayuExecutionEngine;
 #[tokio::main]
 async fn main() -> Result<()> {
     // create local execution context
@@ -15,28 +19,48 @@ async fn main() -> Result<()> {
         CsvReadOptions::new(),
     )
     .await?;
-    let sql1 = "SELECT c1,c3  FROM aggregate_test_100 WHERE c3 < 0 AND c1='a'";
-    let r1 = run_pipeline(&ctx, sql1).await?;
+    let mut executor = VayuExecutionEngine::new();
+    let uuid = 1;
+    let sql1 = "SELECT c1,c3,c13  FROM aggregate_test_100 WHERE c3 < 0 AND c1='a'";
+    let plan1 = get_execution_plan_from_sql(&ctx, sql1).await?;
+    let _ = run_pipeline(&mut executor, plan1, uuid).await?;
 
-    let sql2 = "SELECT c1,c4  FROM aggregate_test_100 WHERE c4 > 0 AND c1='b'";
-    let r2 = run_pipeline(&ctx, sql2).await?;
+    let sql2 = "SELECT c1,c4,c13  FROM aggregate_test_100 WHERE c4 > 0 AND c1='b'";
+    let plan2 = get_execution_plan_from_sql(&ctx, sql2).await?;
+    let _ = run_pipeline(&mut executor, plan2, uuid).await?;
+
+    let blob = executor.store.remove(uuid).unwrap();
+
+    let results = blob.get_records();
+    pretty::print_batches(&results)?;
 
     Ok(())
 }
-
-async fn run_pipeline(ctx: &SessionContext, sql: &str) -> Result<(), DataFusionError> {
+async fn get_execution_plan_from_sql(
+    ctx: &SessionContext,
+    sql: &str,
+) -> Result<Arc<dyn ExecutionPlan>> {
+    // create datafusion logical plan
     let logical_plan = SessionState::create_logical_plan(&ctx.state(), sql).await?;
-    // create datafusion physical plan (trait)
+    // create datafusion physical plan
     let plan = SessionState::create_physical_plan(&ctx.state(), &logical_plan).await?;
-    println!(
-        "Detailed physical plan:\n{}",
-        displayable(plan.as_ref()).indent(true)
-    );
-    // 1 value is not being used, fix it
-    let scheduler_pipeline =
-        vayu::SchedulerPipeline::new(plan, vayu::SchedulerSinkType::RecordBatchStorage);
+    // print datafusion physical plan
+    // println!(
+    //     "Detailed physical plan:\n{}",
+    //     displayable(plan.as_ref()).indent(true)
+    // );
+    Ok(plan)
+}
 
-    let results = vayu::execute(scheduler_pipeline).unwrap();
+async fn run_pipeline(
+    executor: &mut VayuExecutionEngine,
+    plan: Arc<dyn ExecutionPlan>,
+    uuid: i32,
+) -> Result<(), DataFusionError> {
+    let scheduler_pipeline =
+        vayu::SchedulerPipeline::new(plan, vayu::SchedulerSinkType::RecordBatchStorage(uuid));
+
+    let results = executor.execute(scheduler_pipeline).unwrap();
     match results {
         vayu::SchedulerSink::ReturnOutput(results) => pretty::print_batches(&results)?,
         vayu::SchedulerSink::RecordBatchStorage(uuid) => println!("uuid is {uuid}"),
